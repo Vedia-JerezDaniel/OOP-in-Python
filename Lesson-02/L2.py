069111285f1bb50f747db5201fd3ee2571be7236
Daniel H. Vedia Jerez <davedia@hotelbeds.com>
2022-10-14 09:02:43 +0200

    first 3

--- Lesson-02/L2.py
+++ Lesson-02/L2.py
@@ -1,15 +1,21 @@
 class Student:
-    def __init__(self,fName,lName,reg):
+
+    def __init__(self,fName,lName,reg,phone):
         self.fName=fName
         self.lName=lName
         self.reg=reg
         self.email=f'{self.reg.lower()}@uet.edu.pk'
+        self.phone=phone
+
     def welcome(self):
-        return f'Welcome {self.fName} {self.lName}'
+        return f'Welcome {self.fName} {self.lName} to OOP in Python'
 
 ## Main Program ##
-std1=Student('Anwar','Ali','MCT-UET-01') # Object Instantiated and Initialzed
-std2=Student('Akbar','Khan','MCT-UET-02')
+std1=Student('Anwar','Ali','MCT-UET-01','6262') # Object Instantiated and Initialzed
+std2=Student('Akbar','Khan','MCT-UET-02','8060')
 
 print(std1.welcome())
 print(std2.welcome())
+
+print(std1.phone)
+
--- Lesson-03/L3.py
+++ Lesson-03/L3.py
@@ -1,28 +1,39 @@
 class Student:
-    department='Mechatronics'
+    department=['Mechatronics']
     offSubjects=['Mech','LA','ES','CP-II','MOM','Proj']
-    def __init__(self,fName,lName,reg):
+
+    def __init__(self,fName,lName,reg,phone):
         self.fName=fName
         self.lName=lName
         self.reg=reg
         self.email=f'{self.reg.lower()}@uet.edu.pk'
+        self.phone=phone
         self.courses=['Proj']
     
-    def registerSubject(self,*sub):
+    def registerSubject(self, *sub):
         for s in sub:
             if s not in Student.offSubjects:
-                raise ValueError(f'{s} is not offered!')
-            if s in Student.offSubjects and s not in self.courses:
+                raise ValueError(f'Course: {s} is not offered!')
+            if s not in self.courses:
                 self.courses.append(s)
+            if s in ['LA', 'ES']:
+                print(f'{s} is not in {Student.department}')    
+
             
 
 ## Main Program ##
-std1=Student('Anwar','Ali','MCT-UET-01') 
-std2=Student('Akbar','Khan','MCT-UET-02')
-std3=Student('Hamza','Akhtar','MCT-UET-03')
+std1=Student('Anwar','Ali','MCT-UET-01',5612)
+std2=Student('Akbar','Khan','MCT-UET-02',6600)
+std3=Student('Hamza','Akhtar','MCT-UET-03',8521)
 
 std1.registerSubject('CP-II','MOM','MOM')
+std2.registerSubject('LA', 'CP-II')
+std3.registerSubject('Mech','MOM')
+
 print(std1.courses)
+print(std2.courses)
+print(std3.courses)
+
 
 
 
